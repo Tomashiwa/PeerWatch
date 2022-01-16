d251dcbbd49076d79530e8448fb59f44307da069
Tomashiwa <wongqinliang@gmail.com>
2022-01-16 17:51:18 +0800

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+
 import { theme } from "./styles/theme";
+import { ThemeProvider } from "styled-components";
 import { CircularProgress } from "@mui/material";
-import UserContext from "./components/Context/UserContext";
+
 import axios from "axios";
 
+import UserContext from "./components/Context/UserContext";
 import NavBar from "./components/NavBar/NavBar";
 import AppWrapper from "./App.styled";
-import Routes from "./services/Routes";
-import { SERVER_URL } from "./util/url";
 
-const SuspenseSpinner = (
-	<div className="center-screen stretch-height">
-		<CircularProgress color="warning" />
-	</div>
-);
+import Routes from "./services/Routes";
+
+import { SERVER_URL } from "./util/url";
 
 function App() {
 	const [userInfo, setUserInfo] = useState({
@@ -26,36 +24,41 @@ function App() {
 		isLoaded: false,
 	});
 
-	useEffect(() => {
-		let userToken = localStorage.getItem("token");
+	const authLocalToken = async () => {
+		try {
+			let userToken = localStorage.getItem("token");
+			if (!userToken) {
+				localStorage.setItem("token", "");
+				userToken = "";
+			}
 
-		if (!userToken) {
-			localStorage.setItem("token", "");
-			userToken = "";
-		}
-
-		const config = { headers: { Authorization: `Bearer ${userToken}` } };
-		axios
-			.post(`${SERVER_URL}/api/auth/authtoken`, {}, config)
-			.then((res) => {
-				setUserInfo({
-					userId: res.data.userId,
-					displayName: res.data.displayName,
-					email: res.data.email,
-					token: res.data.token,
-					isLoaded: true,
-				});
-			})
-			.catch((err) => {
-				setUserInfo({
-					userId: undefined,
-					displayName: undefined,
-					email: undefined,
-					token: undefined,
-					isLoaded: true,
-				});
+			const config = { headers: { Authorization: `Bearer ${userToken}` } };
+			const res = await axios.post(`${SERVER_URL}/api/auth/authtoken`, {}, config);
+			setUserInfo({
+				userId: res.data.userId,
+				displayName: res.data.displayName,
+				email: res.data.email,
+				token: res.data.token,
+				isLoaded: true,
 			});
-	}, []);
+		} catch (err) {
+			setUserInfo({
+				userId: undefined,
+				displayName: undefined,
+				email: undefined,
+				token: undefined,
+				isLoaded: true,
+			});
+		}
+	};
+
+	const SuspenseSpinner = (
+		<div className="center-screen stretch-height">
+			<CircularProgress color="warning" />
+		</div>
+	);
+
+	useEffect(() => authLocalToken(), []);
 
 	return (
 		<Router>
