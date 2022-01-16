40809c2b1772eebd2cb45cb05757677389ba2b8e
Tomashiwa <wongqinliang@gmail.com>
2022-01-16 16:11:46 +0800

    Handle invalid links and Fix textfield color cchange

--- src/components/VideoLinker/VideoLinker.js
+++ src/components/VideoLinker/VideoLinker.js
@@ -1,12 +1,26 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { TextFieldWrapper } from "./VideoLinker.styled";
 
+const REGEX_YOUTUBE_URL =
+	/^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+
+const ERROR_MSG_INVALID_LINK = "Invalid link. Please try again with another link!";
+
 function VideoLinker({ isDisabled = false, linkCallback }) {
+	const [hasError, setHasError] = useState(false);
 	const inputRef = useRef(null);
 
 	const submitLink = (e) => {
 		e.preventDefault();
-		linkCallback(inputRef.current.value);
+
+		const link = inputRef.current.value;
+		if (!link.match(REGEX_YOUTUBE_URL)) {
+			setHasError(true);
+			return;
+		}
+
+		setHasError(false);
+		linkCallback(link);
 		inputRef.current.value = "";
 	};
 
@@ -17,6 +31,8 @@ function VideoLinker({ isDisabled = false, linkCallback }) {
 				disabled={isDisabled}
 				inputRef={inputRef}
 				placeholder="Enter video link here!"
+				error={hasError}
+				helperText={hasError ? ERROR_MSG_INVALID_LINK : " "}
 			/>
 		</form>
 	);
--- src/components/VideoLinker/VideoLinker.styled.js
+++ src/components/VideoLinker/VideoLinker.styled.js
@@ -4,8 +4,16 @@ import { theme } from "../../styles/theme";
 
 export const TextFieldWrapper = mStyled(TextField)({
 	width: "100%",
-	background: theme.darkGray,
 	".MuiInputBase-root": {
+		background: theme.darkGray,
 		color: `${theme.orange}`,
+	},
+	"input:-webkit-autofill": {
+		"-webkit-box-shadow": `0 0 0 50px ${theme.darkGray} inset`,
+		"-webkit-text-fill-color": theme.orange,
+	},
+	"input:-webkit-autofill:focus": {
+		"-webkit-box-shadow": `0 0 0 50px ${theme.darkGray} inset`,
+		"-webkit-text-fill-color": theme.orange,
 	},
 });
